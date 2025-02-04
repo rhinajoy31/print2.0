180ca9b7d13d821036207dffbb69fcedafb13b48
rhinajoy31 <salinasrhinajoy@gmail.com>
2025-02-04 18:51:51 +0800

    update

--- birthfront2.0.php
+++ birthfront2.0.php
@@ -528,15 +528,6 @@ $html = "
     color: green;
 
 }
-     .output6j {
-    color: green;
-    padding-left: 150px;
-    position: relative;
-    top: -505px;
-    left: -130px; /* Dagdagan ito para lumipat pa kanan */
-    color: green;
-
-}
    
    
   
