c467c7a4252e710cb57fb21539f1c075b13268a7
rhinajoy31 <salinasrhinajoy@gmail.com>
2025-02-08 21:09:16 +0800

    update

--- marriage3.php
+++ marriage3.php
@@ -106,7 +106,7 @@ $html = "
 }
      .vertical-line1 {
             width: 2px; /* Thickness of the line */
-            height: 50px; /* Length of the line */
+            height: 53px; /* Length of the line */
             background-color: rgb(248, 82, 159);
             position: absolute; /* Keeps it inside the container */
             left: 73%; /* Adjust horizontal positioning */
@@ -208,25 +208,421 @@ $html = "
      font-weight: bold;
     font-size: 12px;
 }
+        .outputm2j{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 220px;
+    left: 1px;
+     font-weight: bold;
+}
       .vertical-line2 {
             width: 2px; /* Thickness of the line */
-            height: 300px; /* Length of the line */
+            height: 450px; /* Length of the line */
             background-color: rgb(248, 82, 159);
             position: absolute; /* Keeps it inside the container */
-            left: 60%; /* Adjust horizontal positioning */
+            left: 58.5%; /* Adjust horizontal positioning */
             top: 150px; /* Adjust vertical positioning */
             
         }
             .vertical-line3 {
             width: 2px; /* Thickness of the line */
-            height: 300px; /* Length of the line */
+            height: 450px; /* Length of the line */
             background-color: rgb(248, 82, 159);
             position: absolute; /* Keeps it inside the container */
             left: 15%; /* Adjust horizontal positioning */
             top: 150px; /* Adjust vertical positioning */
             
         }
+   .label3m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 10px;
+     font-weight: bold;
+    font-size: 12px;
 
+}
+     .outputm3a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 255px;
+    left: 10px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+    .outputm3b{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 130px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+     .outputm3c{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 200px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+    .outputm3d{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 280px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+    .outputm3e{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 380px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+      .outputm3f{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 455px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+      .outputm3g{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 525px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+    .outputm3h{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 605px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+     .outputm3i{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 240px;
+    left: 680px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+      .outputm3j{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 260px;
+    left: 1px;
+     font-weight: bold;
+
+}
+     .vertical-line4 {
+            width: 2px; /* Thickness of the line */
+            height: 40px; /* Length of the line */
+            background-color: rgb(248, 82, 159);
+            position: absolute; /* Keeps it inside the container */
+            left: 50%; /* Adjust horizontal positioning */
+            top: 240px; /* Adjust vertical positioning */
+            
+        }
+             .vertical-line5 {
+            width: 2px; /* Thickness of the line */
+            height: 40px; /* Length of the line */
+            background-color: rgb(248, 82, 159);
+            position: absolute; /* Keeps it inside the container */
+            left: 91%; /* Adjust horizontal positioning */
+            top: 240px; /* Adjust vertical positioning */
+            
+        }
+             .label4m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 10px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+          .outputm4a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 120px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+            .outputm4b{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 265px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+       .outputm4c{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 370px;
+     font-weight: bold;
+    font-size: 12px;
+
+}
+      .outputm4d{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 445px;
+     font-weight: bold;
+    font-size: 12px;
+}
+  .outputm4e{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 575px;
+     font-weight: bold;
+    font-size: 12px;
+}
+      .outputm4f{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 280px;
+    left: 660px;
+     font-weight: bold;
+    font-size: 12px;
+}
+    
+      .outputm4g{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 300px;
+    left: 1px;
+     font-weight: bold;
+
+}
+       .label5m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 320px;
+    left: 10px;
+     font-weight: bold;
+       font-size: 12px;
+
+}
+       
+       .outputm5a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 335px;
+    left: 10px;
+     font-weight: bold;
+       font-size: 12px;
+
+}
+        .outputm5b{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 320px;
+    left: 300px;
+     font-weight: bold;
+       font-size: 12px;
+
+}
+        .outputm5c{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 320px;
+    left: 585px;
+     font-weight: bold;
+       font-size: 12px;
+
+}
+        .outputm5d{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 340px;
+    left: 1px;
+     font-weight: bold;
+}
+       .vertical-line6 {
+            width: 2px; /* Thickness of the line */
+            height: 40px; /* Length of the line */
+            background-color: rgb(248, 82, 159);
+            position: absolute; /* Keeps it inside the container */
+            left: 40%; /* Adjust horizontal positioning */
+            top: 320px; /* Adjust vertical positioning */
+            
+        }
+            .vertical-line7 {
+            width: 2px; /* Thickness of the line */
+            height: 40px; /* Length of the line */
+            background-color: rgb(248, 82, 159);
+            position: absolute; /* Keeps it inside the container */
+            left: 79%; /* Adjust horizontal positioning */
+            top: 320px; /* Adjust vertical positioning */
+            
+        }
+                .label6m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 360px;
+    left: 10px;
+     font-weight: bold;
+      font-size: 12px;
+}
+                    .outputm6a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 360px;
+    left: 125px;
+     font-weight: bold;
+      font-size: 11px;
+}
+                   .outputm6b{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 360px;
+    left: 430px;
+     font-weight: bold;
+      font-size: 11px;
+}
+                    .outputm6c{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 380px;
+    left: 1px;
+     font-weight: bold;
+}
+                    .label7m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 400px;
+    left: 5px;
+     font-weight: bold;
+     font-size: 12px;
+       display: block; /* Siguraduhin na block element ito */
+    max-width:0px; /* I-adjust para bumaba sa susunod na linya */
+}
+                  .outputm7a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 410px;
+    left: 1px;
+     font-weight: bold;
+}
+                    .label8m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 5px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                   .outputm8a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top: 30px;
+    left: 1px;
+     font-weight: bold;
+}
+                   .label9m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 5px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                .outputm9a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 120px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                 .outputm9b{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 230px;
+     font-weight: bold;
+     font-size: 12px;
+}
+     
+                 .outputm9c{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 360px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                .outputm9d{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 440px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                  .outputm9e{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 550px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                .outputm9f{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 660px;
+     font-weight: bold;
+     font-size: 12px;
+}
+                 .outputm9g{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:35px;
+    left: 1px;
+     font-weight: bold;
+}
+                 .label10m{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:20px;
+    left: 5px;
+     font-weight: bold;
+      font-size: 12px;
+}
+                   .outputm10a{
+    color: rgb(248, 82, 159);
+    position: absolute; /* Idinagdag ito */
+    top:30px;
+    left: 1px;
+     font-weight: bold;
+}
+  
     </style>
     </head>
     <body>
@@ -246,7 +642,7 @@ $html = "
                                                  <div class='outputm1c'>__________________________________________________________________________________________</div>
                                                       <div class='vertical-line1'></div>
               </div>
-                                                    <div class='section1m'> </div>
+                                                    <div class='section2m'> </div>
                                                       <div class='label2m'> HUSBAND</div>
                                                            <div class='outputm2a'> WIFE</div>
                                                              <div class='outputm2b'> _____________________________________________________________________________</div>
@@ -257,16 +653,71 @@ $html = "
                                                                      <div class='outputm2g'>(First)______________________________________</div>
                                                                       <div class='outputm2h'>(Middle)____________________________________</div>
                                                                        <div class='outputm2i'>(Last)______________________________________</div>
-
+                                                                         <div class='outputm2j'> __________________________________________________________________________________________</div>
                                                          <div class='vertical-line2'></div>
                                                              <div class='vertical-line3'></div>
               </div>
+                                            <div class='section3m'> </div>
+                                              <div class='label3m'>2a.Date of Birth</div>
+                                              <div class='outputm3a'>2b. Age</div>
+                                              <div class='outputm3b'> (Day)</div>
+                                                <div class='outputm3c'> (Month)</div>
+                                                  <div class='outputm3d'> (Year)</div>
+                                                  <div class='outputm3e'> (Age)</div>
+                                                   <div class='outputm3f'> (Day)</div>
+                                                       <div class='outputm3g'> (Month)</div>
+                                                         <div class='outputm3h'> (Year)</div>
+                                                           <div class='outputm3i'> (Age)</div>
+                                                           <div class='outputm3j'> __________________________________________________________________________________________</div>
+                                                              <div class='vertical-line4'></div>
+                                                                   <div class='vertical-line5'></div>
                 </div>
+                   <div class='section4m'> </div>
+                      <div class='label4m'>3.Place of Birth</div>
+                      <div class='outputm4a'>(City/Municipality)</div>
+                       <div class='outputm4b'>(Province)</div>
+                            <div class='outputm4c'>(Country)</div>
+                               <div class='outputm4d'>(City/Municipality)</div>
+                                  <div class='outputm4e'>(Province)</div>
+                                      <div class='outputm4f'>(Country)</div>
+                                      <div class='outputm4g'> __________________________________________________________________________________________</div>
+                </div>
+                <div class='section5m'> </div>
+                      <div class='label5m'>4a.Sex</div>
+                        <div class='outputm5a'>4b.Citizenship</div>
+                           <div class='outputm5b'>(Citizenship)</div>
+                              <div class='outputm5c'>(Citizenship)</div>
+                              <div class='outputm5d'> __________________________________________________________________________________________</div>
+                                   <div class='vertical-line6'></div>
+                                      <div class='vertical-line7'></div>
                   </div>
+                  <div class='section6m'> </div>
+                     <div class='label6m'>5.Residence</div>
+                       <div class='outputm6a'>(House No,.St.,Barangay,City/Municipality,Province,Country)</div>
+                        <div class='outputm6b'>(House No,.St.,Barangay,City/Municipality,Province,Country)</div>
+                          <div class='outputm6c'> __________________________________________________________________________________________</div>
                     </div>
+                     <div class='section7m'> </div>
+                     <div class='label7m'>6.Religion/Religious Sect</div>
+                     <div class='outputm7a'>__________________________________________________________________________________________<div>
                       </div>
+                        <div class='section8m'> </div>
+                     <div class='label8m'>7.Civil Status</div>
+                       <div class='outputm8a'>__________________________________________________________________________________________<div>
                         </div>
-
+                              <div class='section9m'> </div>
+                                   <div class='label9m'>8.Name of Father</div>
+                                      <div class='outputm9a'>(First)</div>
+                                        <div class='outputm9b'>(Middle)</div>
+                                         <div class='outputm9c'>(Last)</div>
+                                          <div class='outputm9d'>(First)</div>
+                                        <div class='outputm9e'>(Middle)</div>
+                                         <div class='outputm9f'>(Last)</div>
+                                          <div class='outputm9g'>__________________________________________________________________________________________<div>
+                                  </div>
+                          <div class='section10m'> </div>
+                                   <div class='label10m'>9.Citizenship</div>
+                                   <div class='outputm10a'>__________________________________________________________________________________________<div>
 </body>
 </html>";
 
